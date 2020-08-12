59b34f160e168ead91109402b50ef155e8c7d253
Sambo18 <47387951+Sambo18@users.noreply.github.com>
2020-08-12 14:46:43 +0100

    dd
    novo comi

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def main():
+    return 'Hello with flask!'
+
+@app.route('/')
+def menu():
+    response = "PyApp Demo \n"
+    response += "1. Seu numero\n"
+    response += "2. Dica do dia\n"
+    response += "3. Sair\n"
+    return response
+
+if __name__ == "__main__":
+    app.run()
